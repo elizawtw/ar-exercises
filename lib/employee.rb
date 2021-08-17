a76853b1ddfae9683a769474af06a72d3259e878
elizawtw <twew02@gmail.com>
2021-08-17 16:35:28 +0000

    exercise 7 done

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 30, less_than_or_equal_to: 100 }
+  validates :store_id, numericality: { greater_than_or_equal_to: 1}
 end
